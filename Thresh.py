fbe139000ae7c86fd03045dae5aafdf0c34e5fd6
Aparajithashri <47823913+Aparajithashri@users.noreply.github.com>
2019-12-04 14:47:30 +0530

    Day 3

---
+++ Thresh.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+"""
+1st argument is the source image, which should be a grayscale image.
+2nd argument is the threshold value which is used to classify the pixel values.
+3rd argument is the maxVal which represents the value to be given if pixel value is more than (sometimes less than) the threshold value.
+4th argument is the different types of threshold.
+"""
+img = cv2.imread('Baby.jpg',0)
+ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+ret,thresh2 = cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV)
+ret,thresh3 = cv2.threshold(img,127,255,cv2.THRESH_TRUNC)
+ret,thresh4 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO)
+ret,thresh5 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO_INV)
+
+titles = ['Original Image','BINARY','BINARY_INV','TRUNC','TOZERO','TOZERO_INV']
+images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
+
+for i in range(6):
+    plt.subplot(2,3,i+1),plt.imshow(images[i],'gray')
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
+
+plt.show()
